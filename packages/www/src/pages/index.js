26b4461b19dd5fc7421c50971397c0fd1322dfca
jacob <jacobcunningham96@yahoo.com>
2020-11-24 15:20:02 -0700

    set up netlify identity widget

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Heading, Button, Flex } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
 
-export default () => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading as="h1">Get shit done</Heading>
-      <Button
-        style={{ backgroundColor: "black", color: "wheat", marginTop: "3px" }}
-        onClick={() => alert("You done clicked me")}
-      >
-        Log in
-      </Button>
-    </Flex>
-  </Container>
-);
+export default () => {
+  useEffect(() => netlifyIdentity.init({}), []);
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Get shit done</Heading>
+        <Button
+          style={{ backgroundColor: "black", color: "wheat", marginTop: "3px" }}
+          onClick={() => netlifyIdentity.open()}
+        >
+          Log in
+        </Button>
+        <Button
+          style={{ backgroundColor: "black", color: "wheat", marginTop: "3px" }}
+          onClick={() => console.log(netlifyIdentity.currentUser())}
+        >
+          WHO AM I???
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
